2a0f8ecf5e980d79f11e4c66b34f416963170166
Juanbyluna <juianfernandesdegoes@gmail.com>
2026-04-19 21:31:53 -0300

    recomeço projeto

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package src;
+
+class Financiamento {
+
+}
+
+class InterfaceUsuario {
+    // Add implementation here
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
